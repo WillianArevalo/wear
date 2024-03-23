695fbb5064864b81595de28fe687956390ab5d0f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-03-23 11:02:10 -0600

    Se cambio el codigo para mostrar el menu en modo responsive

--- js/script.js
+++ js/script.js
@@ -5,16 +5,26 @@ $(document).ready(function(){
     const icon = document.getElementById('icon-hamburger');
     const nav = document.getElementById('navbar');
 
+
     btnHamburger.click(function(){
         if(nav.classList.contains("active")){
-            nav.classList.remove("active");
-            icon.classList.remove("bx-x");
-            icon.classList.add("bx-menu");
-            return;
+           closeNav();
         }else{
-            nav.classList.add("active");
-            icon.classList.remove("bx-menu");
-            icon.classList.add("bx-x");
+            openNav(); 
         }
     });
+
+
+    function openNav() {
+        nav.classList.add("active");
+        icon.classList.remove("bx-menu");
+        icon.classList.add("bx-x");
+    }
+    
+    function closeNav() {
+        nav.classList.remove("active");
+        icon.classList.remove("bx-x");
+        icon.classList.add("bx-menu");
+    }
+
 });
