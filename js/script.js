92ae10d8c5a4e415ead60c593b5be6b8c0cca924
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-03-25 16:09:37 -0600

    Se agrego que cuando el navbar este activo no se pueda hacer scroll en el body

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var btnHamburger = $('#button-hamburger');
     const icon = document.getElementById('icon-hamburger');
     const nav = document.getElementById('navbar');
-
+    const body = document.getElementById('body');
 
     btnHamburger.click(function(){
         if(nav.classList.contains("active")){
@@ -16,12 +16,14 @@ $(document).ready(function(){
 
 
     function openNav() {
+        body.classList.add("active");
         nav.classList.add("active");
         icon.classList.remove("bx-menu");
         icon.classList.add("bx-x");
     }
     
     function closeNav() {
+        body.classList.remove("active");
         nav.classList.remove("active");
         icon.classList.remove("bx-x");
         icon.classList.add("bx-menu");
