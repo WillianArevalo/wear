91fe4ff4e7ae8db8a7260edf86af1dfcb7af539e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-03-28 11:09:17 -0600

    Se ha cambiado el nombre del archivo buttons-size.js a buttons-options.js donde se ha agregado el script de los botones de colores

--- js/buttons-options.js
+++ js/buttons-options.js
@@ -3,10 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     ".product-single__info-sizes-size-item"
   );
 
+  var colors = document.querySelectorAll(
+    ".product-single__info-colors-color-item"
+  );
+
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
       buttons.forEach((button) => {
         button.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+
+  colors.forEach((color) => {
+    color.addEventListener("click", function () {
+      colors.forEach((color) => {
+        color.classList.remove("active");
       });
       this.classList.add("active");
     });
