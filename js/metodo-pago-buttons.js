107a7d241f10917ae8941c8aaa3efb7739dc01f9
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-03-29 15:42:20 -0600

    Se agrego el codigo para poder mostrar el total de los productos en el apartado de resumen de pago y el total del precio en la pagina de payment

--- js/metodo-pago-buttons.js
+++ js/metodo-pago-buttons.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".metodo-pago-button");
   var metodoPagoInput = document.getElementById("metodo-pago");
+  var totalPagar = document.getElementById("total-pagar");
+  var cantidadCart = document.querySelectorAll(".total-productos");
+
+  var productos = JSON.parse(localStorage.getItem("products")) || [];
+  var totalCantidad = 0;
+  var totalPrice = 0;
+
+  productos.forEach((producto) => {
+    var precio = producto.price.replace("$", "");
+    totalPrice += parseFloat(precio) * producto.quantity;
+  });
+
+  productos.forEach((producto) => {
+    totalCantidad += producto.quantity;
+  });
+
+  totalPagar.innerText = "$" + totalPrice.toFixed(2);
+
+  cantidadCart.forEach((total) => {
+    total.innerText = totalCantidad;
+  });
 
   buttons.forEach(function (button) {
     button.addEventListener("click", function () {
